1eb06cbcbb4f5627fa76b0f908655fa5f1c6ecad
Karan Nindra <karannindra@gmail.com>
2026-05-09 17:24:38 -0400

    Model 2 implemented and tested

--- src/portfolio_optimizer/optimization.py
+++ src/portfolio_optimizer/optimization.py
@@ -31,3 +31,51 @@ def baseline_lp(mean_returns, max_weight=0.35):
     portfolio_return = np.dot(mean_returns, weights)
 
     return weights, portfolio_return
+
+def risk_constrained_lp(
+    mean_returns,
+    risk,
+    risk_limit=0.18,
+    max_weight=0.35
+):
+
+    n_assets = len(mean_returns)
+
+    # Objective function
+    c = -mean_returns.values
+
+    # Weight constraints
+    A_weights = np.eye(n_assets)
+    b_weights = np.ones(n_assets) * max_weight
+
+    # Risk constraint
+    A_risk = np.array([risk.values])
+    b_risk = np.array([risk_limit])
+
+    # Combine constraints
+    A_ub = np.vstack([A_weights, A_risk])
+    b_ub = np.concatenate([b_weights, b_risk])
+
+    # Equality constraint
+    A_eq = np.ones((1, n_assets))
+    b_eq = [1]
+
+    # Bounds
+    bounds = [(0, 1)] * n_assets
+
+    result = linprog(
+        c,
+        A_ub=A_ub,
+        b_ub=b_ub,
+        A_eq=A_eq,
+        b_eq=b_eq,
+        bounds=bounds,
+        method="highs"
+    )
+
+    weights = result.x
+
+    portfolio_return = np.dot(mean_returns, weights)
+    portfolio_risk = np.dot(risk, weights)
+
+    return weights, portfolio_return, portfolio_risk
